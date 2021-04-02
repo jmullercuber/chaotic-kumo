6cd244e271573bf6207a3468febad3f0018390fb
Joey Muller <jmullercuber@gmail.com>
2021-04-02 13:20:08 -0600

    draggable transparent window

--- client/main.js
+++ client/main.js
@@ -5,6 +5,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    transparent: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -13,14 +15,24 @@ function createWindow() {
   win.loadFile("index.html");
 }
 
-app.whenReady().then(() => {
-  createWindow();
+// https://stackoverflow.com/a/66068492/2877687
+// electron can't be transparent on linux
+// see issue on github: https://github.com/electron/electron/issues/2170
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+app.disableHardwareAcceleration();
+
+//createWindow need to wait(more than about 100ms) if you want the window to be transparent
+// app.whenReady won't work
+app.on("ready", () => {
+  setTimeout(() => {
+    createWindow();
+
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+  }, 200);
 });
 
 app.on("window-all-closed", () => {
